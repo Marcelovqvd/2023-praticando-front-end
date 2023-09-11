9b4fb1eb5abe9b9d0f950948949837b49ea96d29
Marcelo <marcelovqvd@gmail.com>
2023-09-11 18:11:55 -0300

    form

--- scripts.js
+++ scripts.js
@@ -1,12 +1,1 @@
-const form = document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
 
-const todo = document.querySelector("input");
-
-const myButton = document.querySelector("button");
-
-function handleSubmit(params) {
-  params.preventDefault();
-  myButton.classList.add("disabled");
-}
