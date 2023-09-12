b57a80f7d971300744c2820a1393272904941b2e
Marcelo <marcelovqvd@gmail.com>
2023-09-12 11:29:35 -0300

    wip form

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
+const form = document.querySelector("form");
+const button = document.querySelector("button");
 
+function handleSend(response) {
+  if (response.ok) {
+    form.classList.add("enviado");
+  } else {
+    form.innerHTML = "<p>Não deu </p>";
+    form.classList.add("enviado");
+  }
+}
+
+function handlesubmit(event) {
+  event.preventDefault();
+  button.disabled = true;
+  button.innerText = "Enviando...";
+}
+
+form.addEventListener("submit", handlesubmit);
